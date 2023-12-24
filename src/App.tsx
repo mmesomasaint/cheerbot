2e965d69e2218c460363d3098da2497f2ddca852
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-24 18:40:30 +0100

    Fix: Format with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import Text from "./components/text.tsx"
+import Text from './components/text.tsx'
 
 function App() {
   return (
     <>
-      <header className="px-10 py-4">
-        <div className="">
-        <Text size="xl">Cheerbot</Text>
-      <Text size='sm'>Right words for your messages</Text>
+      <header className='px-10 py-4'>
+        <div className=''>
+          <Text size='xl'>Cheerbot</Text>
+          <Text size='sm'>Right words for your messages</Text>
         </div>
       </header>
     </>
