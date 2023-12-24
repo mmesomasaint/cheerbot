141d7e9e27fe164deecc0182add464def218907b
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-24 18:42:58 +0100

    Fix: Adjust layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ function App() {
   return (
     <>
       <header className='px-10 py-4'>
+        <div className='flex justify-between items-center'>
+          
         <div className=''>
           <Text size='xl'>Cheerbot</Text>
           <Text size='sm'>Right words for your messages</Text>
+        </div>
         </div>
       </header>
     </>
