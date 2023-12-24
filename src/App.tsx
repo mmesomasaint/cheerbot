46d5e1492a6c153e0f95dba2504d276dd909a583
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-24 18:43:08 +0100

    Fix: Format with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,10 @@ function App() {
     <>
       <header className='px-10 py-4'>
         <div className='flex justify-between items-center'>
-          
-        <div className=''>
-          <Text size='xl'>Cheerbot</Text>
-          <Text size='sm'>Right words for your messages</Text>
-        </div>
+          <div className=''>
+            <Text size='xl'>Cheerbot</Text>
+            <Text size='sm'>Right words for your messages</Text>
+          </div>
         </div>
       </header>
     </>
