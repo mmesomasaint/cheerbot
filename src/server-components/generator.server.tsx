54ddc82a871304acbbd9706aacb696e94149adc2
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-24 23:49:54 +0100

    Fix: Update the server component

--- src/server-components/generator.server.tsx
+++ src/server-components/generator.server.tsx
@@ -1,4 +1,21 @@
-export default function Generator() {
+import { CohereClient } from 'cohere-ai'
+
+const cohere = new CohereClient({
+  token: 'kTgRTN0S3ZJS4674O1WF3hkVZbyI2aR3rGjGAWGo',
+})
+
+export default async function Generator({ prompt }: { prompt: string }) {
+  const generate = await cohere.generate({
+    prompt: prompt,
+  })
+
   return (
+    <textarea
+      placeholder='Your generated text...'
+      readOnly
+      title='Your generated text'
+      value={`${generate}`}
+      className='p-4 placeholder:text-black/60 w-full h-full bg-transparent border border-black/20 rounded-md'
+    />
   )
 }
