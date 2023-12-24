199b56cc057b9d129ba76233fbb9a6501410a885
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-24 18:53:58 +0100

    Fix: Format with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import Text from './components/text.tsx'
 function App() {
   return (
     <>
-      <header className='px-10 py-4 border-b border-gray-300/70'>
+      <header className='px-10 py-4 border-b border-gray-300/40'>
         <div className='flex justify-between items-center'>
           <div className=''>
             <Text size='xl'>Cheerbot</Text>
-            <Text size='sm'>Right words for your messages</Text>
+            <Text size='xs'>Right words for your messages</Text>
           </div>
-          <div className='w-12 h-12 flex justify-center items-center border border-gray-300/70 rounded-lg duration-300 hover:scale-110 hover:shadow-lg'>
+          <div className='w-12 h-12 flex justify-center items-center border border-gray-300/40 rounded-lg duration-300 hover:scale-110 hover:shadow-lg'>
             <FaGithub className='text-3xl' />
           </div>
         </div>
