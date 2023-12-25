556763a704782fbb4a7538dd6d7958648212e9a2
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-25 20:38:00 +0100

    Feat: Create the normal generate app

---
+++ src/Generate.tsx
@@ -0,0 +1,34 @@
+import { CohereClient } from 'cohere-ai'
+import { Suspense, useEffect, useState } from 'react'
+import Text from './components/text.tsx'
+
+type PropTypes = {
+  prompt: string
+}
+
+const cohere = new CohereClient({
+  token: 'kTgRTN0S3ZJS4674O1WF3hkVZbyI2aR3rGjGAWGo',
+})
+
+export default function Generate({ prompt }: PropTypes) {
+  const [message, setMessage] = useState<string | null>(null)
+
+  useEffect(() => {
+    const generate = async () => {
+      const response = await cohere.generate({ prompt })
+      setMessage(`${response}`)
+    }
+
+    generate()
+  }, [prompt])
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <article className='p-4 w-full h-full bg-transparent border border-black/20 rounded-md'>
+        <Text size='xs'>
+          {message ? `${message}` : 'Something went wrong. Please try again.'}
+        </Text>
+      </article>
+    </Suspense>
+  )
+}
