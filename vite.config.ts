7011a18c9f4977e8237181fa9d0aa3402d9d6345
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-26 21:34:59 +0100

    Fix: Remove extras

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envPrefix: 'VITE_', // Optional prefix for clarity
-  define: {
-    'process.env': process.env, // Make `process.env` available in dev
-  },
 })
